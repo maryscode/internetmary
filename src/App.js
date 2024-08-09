0795c73f8976575bd83e9eb107c4b3d2fab9f2ce
Mary Chan <mary.chan@area23hc.com>
2024-08-09 16:26:20 -0700

    Fix mobile menu to click to close

--- src/App.js
+++ src/App.js
@@ -1,70 +1,64 @@
 import React, { useState } from 'react';
 import './styles/styles.scss';
-import Background from  './components/Background/Background.js';
-import Nav from  './components/Nav/Nav.js';
-import Footer from  './components/Footer/Footer.js';
-import Portfolio from  './components/Portfolio/Portfolio.js';
-// import chevron from  './images/chevron.svg';
+import Background from './components/Background/Background.js';
+import Nav from './components/Nav/Nav.js';
+import Footer from './components/Footer/Footer.js';
+import Portfolio from './components/Portfolio/Portfolio.js';
 
 function App() {
 
   const [currentScreen, setCurrentScreen] = useState('home');
   const [initialLoad, setInitialLoad] = useState(true);
- //const [showMore, setShowMore] = useState(false);
-  
+  //const [showMore, setShowMore] = useState(false);
+
 
   const handleNavClick = (e) => {
-    window.scrollTo({ top: 0});
-    if (e === 'portfolio'){
+    window.scrollTo({ top: 0 });
+    if (e === 'portfolio') {
       setInitialLoad(false)
     }
     setCurrentScreen(e);
   }
-
-  // const handleClickMore = () => {
-  //   setShowMore(!showMore);
-  // }
-
   return (
     <div className={`App ${currentScreen}`}>
       <Nav currentScreen={currentScreen} onMenuClick={setCurrentScreen} setInitialLoad={setInitialLoad} />
-      
+
       {currentScreen !== 'portfolio' && (
-        <Background bgClass={'home'} currentScreen={currentScreen} />  
+        <Background bgClass={'home'} currentScreen={currentScreen} />
       )}
-        
-        {currentScreen !== 'portfolio' && (
-          <div id="hello" className={initialLoad ? 'initialLoad' : 'loaded'}>
-            
-            <h1 onClick={(e) => {handleNavClick('about')}}>
-                  <span>Mary</span> 
-                  <span>Chan</span>
-            </h1>
-            <p className="animate1">Creative and user-focused full-stack developer. 10+ years of experience. </p>
-              <div className="animate2 flex-center">
-                <button className="btn" onClick={(e) => {handleNavClick('about')}} >About</button>
-                <button className="btn" onClick={(e) => {handleNavClick('portfolio')}} >Portfolio</button>
-              </div>
 
-            {currentScreen === 'about' && (             
-              <div className="content animateFadeInDelay">
-                <h2>Hello, I'm Mary.</h2>
+      {currentScreen !== 'portfolio' && (
+        <div id="hello" className={initialLoad ? 'initialLoad' : 'loaded'}>
 
-                <p>I'm a full-stack developer with expertise in the front-end. I love exploring the creative aspect of programming and I am committed to refining the user experience—both for the end-user interacting with the product and for the developers working with the code.</p>
+          <h1 onClick={(e) => { handleNavClick('about') }}>
+            <span>Mary</span>
+            <span>Chan</span>
+          </h1>
+          <p className="animate1">Creative and user-focused full-stack developer. 10+ years of experience. </p>
+          <div className="animate2 flex-center">
+            <button className="btn" onClick={(e) => { handleNavClick('about') }} >About</button>
+            <button className="btn" onClick={(e) => { handleNavClick('portfolio') }} >Portfolio</button>
+          </div>
 
-                {/* <p>With a background in digital advertising, I've worked on everything from large-scale digital marketing campaigns with a corporate team to independently designing and developing websites from the ground up. No matter the environment, once I start coding, my goals are always the same: make sure it looks good, works on every major browser, and write code in such a way so that my future self won't hate me (i.e. organized, self-evident, and scalable).</p> */}
+          {currentScreen === 'about' && (
+            <div className="content animateFadeInDelay">
+              <h2>Hello, I'm Mary.</h2>
 
-                <p>Over a decade in the industry has honed my ability to quickly learn and adapt to new technologies. Above all, I believe in having a strong foundation in the fundamentals of HTML5, CSS, and JavaScript&mdash;I see everything else as a flavor of libraries and tools for different use cases.</p>
+              <p>I'm a full-stack developer with expertise in the front-end. I love exploring the creative aspect of programming and I am committed to refining the user experience—both for the end-user interacting with the product and for the developers working with the code.</p>
 
-                <p>Today, I'm seeking to focus my front-end skills on collaborating with forward-thinking designers and meticulous engineers to build a world-class product that will positively impact culture.</p>
+              {/* <p>With a background in digital advertising, I've worked on everything from large-scale digital marketing campaigns with a corporate team to independently designing and developing websites from the ground up. No matter the environment, once I start coding, my goals are always the same: make sure it looks good, works on every major browser, and write code in such a way so that my future self won't hate me (i.e. organized, self-evident, and scalable).</p> */}
 
-                <p>When I'm not coding, I can be found <a href="https://stringsmagazine.com/a-bow-of-humility-setting-aside-my-pride-helped-embrace-my-childlike-enthusiasm-for-music/" target='_blank' rel="noreferrer">playing the violin</a> with my chamber ensemble, going on a run,  deep-diving into a curiosity on YouTube and Wiki, or eating a pizza.</p>
+              <p>Over a decade in the industry has honed my ability to quickly learn and adapt to new technologies. Above all, I believe in having a strong foundation in the fundamentals of HTML5, CSS, and JavaScript&mdash;I see everything else as a flavor of libraries and tools for different use cases.</p>
+
+              <p>Today, I'm seeking to focus my front-end skills on collaborating with forward-thinking designers and meticulous engineers to build a world-class product that will positively impact culture.</p>
+
+              <p>When I'm not coding, I can be found <a href="https://stringsmagazine.com/a-bow-of-humility-setting-aside-my-pride-helped-embrace-my-childlike-enthusiasm-for-music/" target='_blank' rel="noreferrer">playing the violin</a> with my chamber ensemble, going on a run,  deep-diving into a curiosity on YouTube and Wiki, or eating a pizza.</p>
 
               {/* <p className='flex-end'><button className={`toggleLink ${showMore ? 'less' : 'more'}`} onClick={handleClickMore}>
                 {showMore ? 'Less on my background' : 'More on my background'} <img src={chevron} />
               </button></p> */}
-            
-            {/* {showMore && (
+
+              {/* {showMore && (
               <div className="sm-txt">
                 <h3>My Coding Journey</h3>
                 <p>Coding started off as a hobby back when websites were made with tables. I saw building websites as a means to an end. As a kid, I built a website to exhibit my cartoon drawings. As a teenager, I customized blogs to showcase my creative writing. With the help of online tutorials (and strict parents who kept me home with nothing else to do), I honed my coding skills and kept going.</p>
@@ -84,24 +78,24 @@ function App() {
               </div>
             )}
              */}
-                  {/* <h3>Website Details</h3>
+              {/* <h3>Website Details</h3>
                   <p>This site was developed with React and SCSS. The designed is based on my aura portrait. </p> */}
 
-                <div className="flex-start mt-40 ml-0">
-                  <button className="btn ml-0" onClick={(e) => {handleNavClick('portfolio')}} >Portfolio</button>
-                  <a className="btn" target="_blank" href="https://www.linkedin.com/in/chanmary" rel="noreferrer">LinkedIn</a>
-                </div>                
-                
+              <div className="flex-start mt-40 ml-0">
+                <button className="btn ml-0" onClick={(e) => { handleNavClick('portfolio') }} >Portfolio</button>
+                <a className="btn" target="_blank" href="https://www.linkedin.com/in/chanmary" rel="noreferrer">LinkedIn</a>
               </div>
-            )}
-          </div>
-        )}
-      
-      { currentScreen === 'portfolio' && (
+
+            </div>
+          )}
+        </div>
+      )}
+
+      {currentScreen === 'portfolio' && (
         <Portfolio />
       )}
 
-      { currentScreen === 'portfolio' && (
+      {currentScreen === 'portfolio' && (
         <Footer currentScreen={currentScreen} onMenuClick={setCurrentScreen} />
       )}
     </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,24 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './Nav.module.scss';
 import Menu from '../Menu/Menu.js';
-export default function Nav({currentScreen, onMenuClick, setInitialLoad}) {
+export default function Nav({ currentScreen, onMenuClick, setInitialLoad }) {
 
     const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
 
     const handleOnClick = (e) => {
-        window.scrollTo({ top: 0});
+        window.scrollTo({ top: 0 });
         onMenuClick(e);
         setIsOpen(false);
-        
+
     }
     const handleToggle = (e) => {
-        
         setIsOpen(prevState => !prevState);
     }
+
+    const handleClickOutside = (event) => { // check if nav exists AND if the target clicked is NOT inside nav
+        if (menuRef.current && !menuRef.current.contains(event.target)){
+            setIsOpen(false);
+        }
+    }
+
+    useEffect(() => { // Event listener: remove when nav component is unmounted.
+        if(isOpen){
+            window.addEventListener('mousedown', handleClickOutside);
+        } else {
+            window.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            window.removeEventListener('mousedown', handleClickOutside);
+        };
+    },[isOpen])    
     return (
-        
-         <div id={styles.nav} className={ currentScreen === 'home' ? styles.placeholder : styles.interior}>
-            <div id={styles.logo} onClick={(e) => {handleOnClick('home')}}>
+
+        <div id={styles.nav} className={currentScreen === 'home' ? styles.placeholder : styles.interior}>
+            <div id={styles.logo} onClick={(e) => { handleOnClick('home') }}>
                 <span></span>
                 <span></span>
                 <span></span>
@@ -26,16 +44,16 @@ export default function Nav({currentScreen, onMenuClick, setInitialLoad}) {
                 <span></span>
                 <span></span>
                 <span></span>
-                <span></span>                
+                <span></span>
             </div>
             <button type='button' aria-label="Toggle navigation" onClick={handleToggle} className={`${styles.menuButton} ${isOpen ? styles.buttonActive : ''}`}>
                 <span></span>
                 <span></span>
                 <span></span>
             </button>
-            <div className={`${styles.navMenu} ${isOpen ? styles.showMenu : ''}`}>
+            <div className={`${styles.navMenu} ${isOpen ? styles.showMenu : ''}`}  ref={menuRef}>
                 <Menu setInitialLoad={setInitialLoad} currentScreen={currentScreen} onMenuClick={onMenuClick} setIsOpen={setIsOpen} />
-            </div> 
+            </div>
         </div>
     )
 
